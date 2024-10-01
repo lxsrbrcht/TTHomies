0ac51743aadbc7f13d46945ca951e512050cb3f8
Antoine Heu <antoine.heu@gmail.com>
2024-10-01 14:34:12 +0200

    character model changed with validates and some views started

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,12 @@
 class Character < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  RACES = %w[human elf dwarf halfling gnome half-elf half-orc tiefling aasimar dragonborn goliath]
+
+  validates :name, presence: true
+  validates :dnd_class, presence: true
+  validates :dnd_subclass, presence: true
+  validates :race, inclusion: { in: RACES }
+  validates :bio, presence: true, length: { minimum: 50, maximum: 700 }
 end
