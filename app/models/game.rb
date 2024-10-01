7ddde6477d17dce638e992fcaf7ecdfa5b5a61f7
Antoine Heu <antoine.heu@gmail.com>
2024-10-01 15:58:49 +0200

    new form started, changes made to the character card and model game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,7 @@ class Game < ApplicationRecord
   has_many :dnd_class
   has_many :dnd_subclass
 
-  validates :system, inclusion: { in: ["D&D 5", "Pathfinder"] }
+  SYSTEMS = ["D&D 5", "Pathfinder"]
+
+  validates :system, inclusion: { in: SYSTEMS }
 end
